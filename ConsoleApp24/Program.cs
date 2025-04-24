274c90bc117493a40c12b872002780950f28b654
exuteboll <exuteboll4@gmail.com>
2025-04-24 11:05:18 +0300

    Add files via upload

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        public static double Average(double m, double n)
+        {
+            double sum = 0;
+            int count = 0;
+            for (double i = m; i <= n; i++)
+            {
+                sum += i;
+                count++;
+            }
+            return sum / count;
+        }
+        public delegate double AverageDelegate(double m, double n);
+
+        static void Main(string[] args)
+        {
+            AverageDelegate avg = new AverageDelegate(Average);
+
+            double res = avg(10, 20);
+            Console.WriteLine(res);
+
+            double res1 = avg(100, 200);
+            Console.WriteLine(res1);
+
+            double res2 = avg(300, 600);
+            Console.WriteLine(res2);
+            Console.ReadKey();
+        }
+    }
+}
